ee024c9ec72c7e240d0de97b4e015fca4fec24df
felipe <felipepmaragno@gmail.com>
2018-07-05 19:19:57 -0400

    Adiciona funcionalidade dos timers 25 e 5 min
    - Criação das funções pomodoroCountDown(), descansoCountDown() e timer()
    para contagem regressiva do tempo especificado, atualizando timer
    enquanto o tempo passa.
    - Criação da variável timeOut para ser usada com as funções
    setInterval() e clearInterval().
    - Adiciona os eventListeners da ação click nos botões de star, para
    pomodoro e descanso.

--- js/script.js
+++ js/script.js
@@ -1,3 +1,6 @@
+//inicializa var global para usar com setInterval e clearInterval
+var timeOut;
+
 window.onload = function() {
     var descansoDisplay = document.querySelector('.descanso-display');
     var pomDisplay = document.querySelector('.pom-display');
@@ -10,5 +13,70 @@ window.onload = function() {
     tempoLivre.innerHTML = 5
     var pomodoroMinutos = pomodoro.textContent;
     var descansoMinutos = tempoLivre.textContent;
+
+    function pomodoroCountDown() {
+        var minutes = pomodoroMinutos - 1;
+        var seconds = 60;
+        //var total -> tempo total em segs
+        var total = (minutes * 60) + seconds;
+        timeOut = setInterval(timer, 1000);
+
+        function timer() {
+            seconds = seconds - 1;
+            total = total - 1;
+            if (seconds === -1) {
+                minutes = minutes - 1;
+                seconds = 59;
+            }
+            var display = document.querySelector('.tempo-display');
+            display.innerHTML = (minutes < 10 ? "0" + minutes.toString() : minutes) + ':' + (seconds < 10 ? "0" + seconds.toString() : seconds);
+            if (total === 0) {                                
+                clearInterval(timeOut);
+                pomDisplay.style.display = 'none';                 
+            }
+        }
     }
+
+    function descansoCountDown() {
+        var minutes = descansoMinutos - 1;
+        var seconds = 60;        
+        //var total -> tempo total em segs
+        var total = (minutes * 60) + seconds;
+        descansoDisplay.style.display = 'inline-block';
+        pomDisplay.style.display = 'none';                
+        timeOut = setInterval(timer, 1000);
+
+        function timer() {
+            seconds = seconds - 1;
+            total = total - 1;
+            if (seconds === -1) {
+                minutes = minutes - 1;
+                seconds = 59;
+            }
+            var display = document.querySelector('.tempo-display');
+            display.innerHTML = (minutes < 10 ? "0" + minutes.toString() : minutes) + ':' + (seconds < 10 ? "0" + seconds.toString() : seconds);
+            if (total === 0) {
+                //limpa tempo descanso 
+                clearInterval(timeOut);
+            }
+        }
+    }
+
+    start.addEventListener('click', function() {
+        
+        descansoDisplay.style.display = 'none';
+        start.style.display = 'none';
+        startDescanso.style.display = 'none';    
+        pomodoroCountDown();
+    });
+
+    startDescanso.addEventListener('click', function() {
+        
+        descansoDisplay.style.display = 'none';
+        start.style.display = 'none';
+        startDescanso.style.display = 'none';    
+        descansoCountDown();
+    });
+
+}
 
